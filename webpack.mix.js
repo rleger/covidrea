858c401dc2676e60a0e708b76a657fc5495d8190
Romain Léger <legerrom@gmail.com>
2020-03-20 11:49:56 +0100

    format

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
 
 mix.postCss('resources/css/main.css', 'public/css', [
-  require('tailwindcss'),
+    require('tailwindcss'),
 ])
