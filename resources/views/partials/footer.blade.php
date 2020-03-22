f707535d6a06d83ee3114bef6e8717ecbc547601
Romain Léger <legerrom@gmail.com>
2020-03-22 16:38:55 +0100

    copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,25 @@
 <div class="bg-white">
     <div class="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav class="-mx-5 -my-2 flex flex-wrap justify-center">
-            <div class="px-5 py-2">
-                <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    About
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Accessibility
-                </a>
-            </div>
-            <div class="px-5 py-2">
-                <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900">
-                    Partners
-                </a>
-            </div>
-        </nav>
+        {{-- <nav class="-mx-5 -my-2 flex flex-wrap justify-center"> --}}
+            {{-- <div class="px-5 py-2"> --}}
+                {{-- <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900"> --}}
+                    {{-- About --}}
+                {{-- </a> --}}
+            {{-- </div> --}}
+            {{-- <div class="px-5 py-2"> --}}
+                {{-- <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900"> --}}
+                    {{-- Accessibility --}}
+                {{-- </a> --}}
+            {{-- </div> --}}
+            {{-- <div class="px-5 py-2"> --}}
+                {{-- <a href="#" class="text-base leading-6 text-gray-500 hover:text-gray-900"> --}}
+                    {{-- Partners --}}
+                {{-- </a> --}}
+            {{-- </div> --}}
+        {{-- </nav> --}}
         <div class="mt-8">
             <p class="text-center text-base leading-6 text-gray-400">
-                &copy; 2020 All rights reserved.
+                &copy; 2020 Romain Léger. All rights reserved.
             </p>
         </div>
     </div>
