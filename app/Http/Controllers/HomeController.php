9eb6932e153632d7f78646d4c470c37afb9c9e02
Romain Léger <legerrom@gmail.com>
2020-03-24 23:17:12 +0100

    check user has a hospital

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,6 +17,8 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
+        $this->middleware('checkuserhasahospital');
     }
 
     /**
