953e156ed2b30942a497f7ea6461c458e5c08d42
Romain Léger <legerrom@gmail.com>
2020-03-24 14:09:11 +0100

    updated time tweaks

--- resources/views/etablissement/index.blade.php
+++ resources/views/etablissement/index.blade.php
@@ -50,10 +50,12 @@
                                                 <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/>
                                                 </svg>
+                                                <span class="hidden sm:inline">
+                                                    mis à jour&nbsp;
+                                                </span>
                                                 <span>
-
                                                     {{-- <time datetime="2020-01-07">January 7, 2020</time> --}}
-                                                    {{ $etablissement->service()->latest()->first()->created_at->diffForHumans() }}
+                                                    {{ $etablissement->service()->orderBy('updated_at', 'DESC')->first()->updated_at->diffForHumans() }}
                                                 </span>
                                             @endif
                                         </div>
--- resources/views/etablissement/show.blade.php
+++ resources/views/etablissement/show.blade.php
@@ -41,6 +41,14 @@
                                 {{ $service->place_totales }} totales
                             </span>
                         </div>
+                        <div class="mt-2 flex items-center text-xs leading-5 text-gray-500 sm:mt-2">
+                            <svg class="flex-shrink-0 mr-1.5 h-4 w-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/>
+                            </svg>
+                            <span>
+                                <span class="hidden sm:inline">mis à jour </span>{{ $service->updated_at->diffForHumans() }}
+                            </span>
+                        </div>
                         </dd>
                     </div>
                 @endforeach
