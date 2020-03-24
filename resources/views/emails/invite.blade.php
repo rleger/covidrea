b020f62d2e61b43914b594910886f04c0e9ebd84
Romain Léger <legerrom@gmail.com>
2020-03-24 11:02:22 +0100

    email template

--- resources/views/emails/invite.blade.php
+++ resources/views/emails/invite.blade.php
@@ -1,19 +1,20 @@
 @component('mail::message')
 {{-- # {{ $details['title'] }} --}}
 
-# Activez votre compte Covid-moi-un-lit.com
+# Activez votre compte covid-moi-un-lit.com
+
 
 Bonjour,
 
-Vous avez été invité à rejoindre **gratuitement** l'application [covid-moi-un-lit.com](https://covid-moi-un-lit.com) afin de vous aider à trouver des lits plus facilement pour vos malades covid+ (intubés ou non intubés).
+Vous avez été invité à rejoindre **gratuitement** l'application covid-moi-un-lit.com afin de vous aider à trouver des lits plus facilement pour vos malades covid+ (intubés ou non intubés).
 
-Vous pourrez également mettre à jour en temps réel le nombre de lits disponibles pour votre unité.
+Cette application est **collaborative**.
 
 @component('mail::button', ['url' => route('invite.accept', ['token' => $invite->token, 'etablissement_id' => $invite->etablissement_id, 'email' => $invite->email ]) ])
 Activer mon compte
 @endcomponent
 
-Vous pourrez également indiquer et mettre à jour le nombre de lits disponibles pour votre unité.
+Nous comptons sur vous pour mettre à jour en temps réel le nombre de lits disponibles pour votre unité.
 
-{{ config('app.name') }}
+L'équipe covid moi un lit
 @endcomponent
