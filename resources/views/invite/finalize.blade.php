2fe9d7314b21eb0fbb21cfaefdc60363ead2a1ce
Romain Léger <legerrom@gmail.com>
2020-03-25 17:00:17 +0100

    autocomplete

--- resources/views/invite/finalize.blade.php
+++ resources/views/invite/finalize.blade.php
@@ -50,7 +50,7 @@
                                 </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                                     <div class="max-w-xs rounded-md shadow-sm">
-                                        <input name="name" id="name" maxlength=30 value="{{ old('name') }}" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" autofocus />
+                                        <input name="name" id="name" autocomplete="name" maxlength=30 value="{{ old('name') }}" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" autofocus />
                                     </div>
                                 </div>
                             </div>
@@ -61,18 +61,18 @@
                                 </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                                     <div class="max-w-xs rounded-md shadow-sm">
-                                        <input name="email" type="email" id="email" value="{{ old('email') ?? app('request')->input('email') }}" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input name="email" type="email" id="email" autocomplete="email" value="{{ old('email') ?? app('request')->input('email') }}" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                 </div>
                             </div>
 
                             <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="phone_mobile" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Téléphone portable
+                                    Téléphone portable
                                 </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                                     <div class="max-w-xs rounded-md shadow-sm">
-                                        <input name="phone_mobile" type="tel" value="{{ old('phone_mobile') }}" id="phone_mobile" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <input name="phone_mobile" type="tel" autocomplete="tel" value="{{ old('phone_mobile') }}" id="phone_mobile" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                     </div>
                                     <p class="mt-2 text-sm text-gray-500">Ne sera PAS rendu public mais uniquement pour vous contacter par SMS en cas de nécessité.</p>
                                 </div>
@@ -80,7 +80,7 @@
 
                             <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="password" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Mot de passe
+                                    Mot de passe
                                 </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                                     <div class="max-w-lg rounded-md shadow-sm">
@@ -92,7 +92,7 @@
 
                             <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="password" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Confirmez mot de passe
+                                    Confirmez mot de passe
                                 </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                                     <div class="max-w-lg rounded-md shadow-sm">
@@ -100,16 +100,15 @@
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                     <div class="mt-8 border-t border-gray-200 pt-8 sm:mt-5 sm:pt-10">
                         <div>
                             <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Services
+                                Services
                             </h3>
                             <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
-                            Selectionnez les services dans lesquels vous intervenez, vous aurez le droit de modifier les places pour ces services uniquement.
+                                Selectionnez les services dans lesquels vous intervenez, vous aurez le droit de modifier les places pour ces services uniquement.
                             </p>
                         </div>
                         <div class="mt-6 sm:mt-5">
@@ -118,24 +117,24 @@
                                     <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
                                         <div>
                                             <legend class="text-base leading-6 font-medium text-gray-900 sm:text-sm sm:leading-5 sm:text-gray-700">
-                                            Services
+                                                Services
                                             </legend>
                                         </div>
                                         <div class="mt-4 sm:mt-0 sm:col-span-2">
                                             <div class="max-w-lg">
-                                            @foreach($services as $service_key => $service)
-                                                <div class="{{ !$service_key ? 'relative flex items-start' : ''}} mt-4">
-                                                    <div class="relative flex items-start">
-                                                        <div class="absolute flex items-center h-5">
-                                                            <input id="candidates" name="service[{{$service->id}}]" type="checkbox"  class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
-                                                        </div>
-                                                        <div class="pl-7 text-sm leading-5">
-                                                            <label for="candidates" class="font-medium text-gray-700">{{ $service->name }}</label>
-                                                            {{-- <p class="text-gray-500">{{ $service->type}}</p> --}}
+                                                @foreach($services as $service_key => $service)
+                                                    <div class="{{ !$service_key ? 'relative flex items-start' : ''}} mt-4">
+                                                        <div class="relative flex items-start">
+                                                            <div class="absolute flex items-center h-5">
+                                                                <input id="candidates" name="service[{{$service->id}}]" type="checkbox"  class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" />
+                                                            </div>
+                                                            <div class="pl-7 text-sm leading-5">
+                                                                <label for="candidates" class="font-medium text-gray-700">{{ $service->name }}</label>
+                                                                {{-- <p class="text-gray-500">{{ $service->type}}</p> --}}
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            @endforeach
+                                                @endforeach
                                             </div>
                                         </div>
                                     </div>
@@ -146,16 +145,16 @@
                 </div>
                 <div class="mt-8 border-t border-gray-200 pt-5">
                     <div class="flex justify-end">
-                    {{-- <span class="inline-flex rounded-md shadow-sm"> --}}
-                    {{-- <button type="button" class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"> --}}
-                    {{-- Annuler --}}
-                    {{-- </button> --}}
-                    {{-- </span> --}}
-                <span class="ml-3 inline-flex rounded-md shadow-sm">
-                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                    Enregistrer
-                    </button>
-                </span>
+                        {{-- <span class="inline-flex rounded-md shadow-sm"> --}}
+                            {{-- <button type="button" class="py-2 px-4 border border-gray-300 rounded-md text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"> --}}
+                                {{-- Annuler --}}
+                                {{-- </button> --}}
+                            {{-- </span> --}}
+                        <span class="ml-3 inline-flex rounded-md shadow-sm">
+                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Enregistrer
+                            </button>
+                        </span>
                     </div>
                 </div>
             </form>
