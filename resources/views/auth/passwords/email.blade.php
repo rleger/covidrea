6439f96ef17a4447dbaeee84c71b3877a50c1bd1
Romain Léger <legerrom@gmail.com>
2020-03-25 11:42:58 +0100

    new email reset form

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="row justify-content-center"><div class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <h2 class="mt-2 mb-4 text-xl leading-9 font-extrabold text-gray-900">
+                        Réinitialisez votre mot de passe
+                    </h2>
 
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
+                        <div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            {{-- Success message --}}
+                            @if (session('status'))
+                                <div class="border border-green-400 rounded bg-green-100 mt-4 px-4 py-3 text-green-700">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            @if ($errors->any())
+                                <div role="alert">
+                                    <div class="border border-red-400 rounded bg-red-100 my-4 px-4 py-3 text-red-700">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                </div>
+                            @endif
+
+                            <label for="email" class="mt-4 block text-sm font-medium leading-5 text-gray-700">
+                                Adresse email
+                            </label>
+                            <div class="mt-1 rounded-md shadow-sm">
+
+                                <input id="email" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                        <div class="mt-6">
+                            <span class="block w-full rounded-md shadow-sm">
+                                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                    Envoyer le lien
                                 </button>
-                            </div>
+                            </span>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+        </div></div>
+    @endsection
