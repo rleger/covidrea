ebb71ea7cc12028e1ba345175fcb1fdd83c45c44
Romain Léger <legerrom@gmail.com>
2020-03-26 16:46:37 +0100

    missing tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,14 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script> --}}
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
         <!-- Styles -->
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+        <!-- App -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161631692-1"></script>
@@ -59,12 +58,12 @@
                                         {{-- <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"> --}}
                                             {{-- <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24"> --}}
                                                 {{-- <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> --}}
-                                            {{-- </svg> --}}
-                                        {{-- </button> --}}
+                                                {{-- </svg> --}}
+                                            {{-- </button> --}}
                                         <div  class="ml-3 relative">
                                             <div>
 
-                                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</button>
+                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</button>
 
                                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                                                     @csrf
@@ -118,7 +117,7 @@
                 @endif
                 <main>
                     <div class="max-w-7xl mx-auto py-2 sm:py-6 sm:px-6 lg:px-8">
-                        <!-- Replace with your content -->
+                    <!-- Replace with your content -->
                         <div class="sm:px-0">
                             @yield('content')
                         </div>
@@ -127,5 +126,6 @@
                 </main>
             </div>
             @include('partials.footer')
+        </div>
     </body>
 </html>
