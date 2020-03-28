39614677156144ddba623240e1fc3b3c69b848a2
Romain Léger <legerrom@gmail.com>
2020-03-28 17:17:24 +0100

    PSR-4

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -29,14 +29,16 @@ class AuthServiceProvider extends ServiceProvider
         // only authorize service editing when the service actually belongs to the logged-in user
         Gate::define('edit-service', function ($user, $service) {
             $authorized = $user->services()->get()->contains($service->id);
-            Log::info("Is user " . $user->id . " authorized to edit service " . $service->id . " => " . $authorized);
+            Log::info('Is user '.$user->id.' authorized to edit service '.$service->id.' => '.$authorized);
+
             return $authorized;
         });
 
         // only authorize etablissement editing when the logged-in user is responsible for this etablissement
         Gate::define('edit-etablissement', function ($user, $etablissement) {
             $authorized = $etablissement->user_id == $user->id;
-            Log::info("Is user " . $user->id . " authorized to edit etablissement " . $etablissement->id . " => " . $authorized);
+            Log::info('Is user '.$user->id.' authorized to edit etablissement '.$etablissement->id.' => '.$authorized);
+
             return $authorized;
         });
     }
