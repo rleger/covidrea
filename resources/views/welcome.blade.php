5bb3730d883dfc4d0e1f1441fd7c71e7c943f549
Romain Léger <legerrom@gmail.com>
2020-03-28 14:06:28 +0100

    analytics on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-161631692-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-161631692-1');
+        </script>
 </head>
 
 <body>
