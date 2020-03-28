edd2d2d7d17cfb0e487dfb2ed33cb5895550e7c2
bdahon <benjamin.dahon@gmail.com>
2020-03-28 21:58:50 +0100

    Added update for etablissement model

--- app/Http/Controllers/UserEtablissementController.php
+++ app/Http/Controllers/UserEtablissementController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Etablissement;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class UserEtablissementController extends Controller
 {
@@ -30,9 +32,26 @@ class UserEtablissementController extends Controller
     }
 
 
-    public function update(User $user, Etablissement $etablissement)
+    public function update(Request $request, User $user, Etablissement $etablissement)
     {
         Gate::authorize('edit-etablissement', $etablissement);
+        Log::info("About to update etablissement " . $etablissement->id);
 
+        $request->session()->flash('etablissement_id', $etablissement->id);
+
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'type' => 'required|string',
+        ]);
+
+        // Model update
+        $etablissement->update($validatedData);
+
+        // Back to the view
+        return back()
+            ->withInput()
+            ->with([
+                'status' => 'Nombre de lits mis à jour',
+        ]);
     }
 }
--- resources/views/user/etablissement/edit.blade.php
+++ resources/views/user/etablissement/edit.blade.php
@@ -4,131 +4,80 @@
 
 @section('content')
     <div class="bg-white overflow-hidden shadow rounded-lg">
-        <form action="" method="POST">
+        <form action="/user/etablissement/{{$etablissement->id}}/update" method="POST">
             @method('PATCH')
             @csrf
-            <div class="px-4 py-5 text-lg leading-6 font-medium text-indigo-600 sm:px-6">
-                {{ $etablissement->name }}
+            {{-- Display errors if any --}}
+            @if ($errors->any() && $etablissement->id == session('etablissement_id'))
+                <div role="alert">
+                    <div class="border border-red-400 rounded bg-red-100 mt-4 px-4 py-3 text-red-700">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            @endif
+            <div class="px-4 py-5 border-b border-gray-200 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-indigo-600">
+                    {{ $etablissement->name }}
+                </h3>
+                <div class="mt-1 flex items-center text-sm leading-5 text-gray-500">
+                  <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h8a2 2 0 012 2v12a1 1 0 110 2h-3a1 1 0 01-1-1v-2a1 1 0 00-1-1H9a1 1 0 00-1 1v2a1 1 0 01-1 1H4a1 1 0 110-2V4zm3 1h2v2H7V5zm2 4H7v2h2V9zm2-4h2v2h-2V5zm2 4h-2v2h2V9z" clip-rule="evenodd"/>
+                    </svg>
+                    {{ $etablissement->type }}
+                </div>
             </div>
             <div class="px-4 pb-8">
                 <div class="border-t border-gray-200">
                     <div class="mt-6 sm:mt-5">
                         <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start">
-                            <label for="first_name" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                First name
+                            <label for="name" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+                                Nom
                             </label>
                             <div class="mt-1 sm:mt-0 sm:col-span-2">
                                 <div class="max-w-xs rounded-md shadow-sm">
-                                    <input id="first_name" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    <input name="name" id="name" value="{{ $etablissement->name }}" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                 </div>
                             </div>
                         </div>
-
                         <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="last_name" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Last name
+                            <label for="type" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
+                                Type
                             </label>
                             <div class="mt-1 sm:mt-0 sm:col-span-2">
                                 <div class="max-w-xs rounded-md shadow-sm">
-                                    <input id="last_name" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="email" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Email address
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-lg rounded-md shadow-sm">
-                                    <input id="email" type="email" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="country" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Country / Region
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-xs rounded-md shadow-sm">
-                                    <select id="country" class="block form-select w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5">
-                                        <option>United States</option>
-                                        <option>Canada</option>
-                                        <option>Mexico</option>
+                                    <select name="type" id="type" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />>
+                                        <option value="public" {{ $etablissement->type == "public" ? "selected" : "" }}>Public</option>
+                                        <option value="prive" {{ $etablissement->type == "prive" ? "selected" : "" }}>Privé</option>
+                                        <option value="temporaire" {{ $etablissement->type == "temporaire" ? "selected" : "" }}>Temporaire</option>
                                     </select>
+                                    
                                 </div>
                             </div>
                         </div>
 
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="street_address" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                Street address
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-lg rounded-md shadow-sm">
-                                    <input id="street_address" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="city" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                City
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-xs rounded-md shadow-sm">
-                                    <input id="city" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="state" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                State / Province
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-xs rounded-md shadow-sm">
-                                    <input id="state" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                            <label for="zip" class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2">
-                                ZIP / Postal
-                            </label>
-                            <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                <div class="max-w-xs rounded-md shadow-sm">
-                                    <input id="zip" class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                                </div>
-                            </div>
-                        </div>
+                      
                     </div>
                 </div>
-
-
-
-
-
-
-
-
-
-
-
             </div>
             <div class="border-t border-gray-200 px-4 py-4 sm:px-6">
-                <span class="inline-flex rounded-md shadow-sm">
-                    <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
-                        {{ __('Save') }}
-                    </button>
-                </span>
-                <span class="inline-flex rounded-md shadow-sm">
-                    <button type="button" class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-200 transition ease-in-out duration-150">
-                        {{ __('Cancel') }}
-                    </button>
-                </span>
+                <div class="sm:col-span-2">
+                    <span class="inline-flex rounded-md shadow-sm">
+                        {{-- Valider --}}
+                        <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                            {{ __('Save') }}
+                        </button>
+
+                        {{-- Annuler --}}
+                        <a href="{{ Request::url() }}" class="ml-2 inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-200 transition ease-in-out duration-150">
+                            {{ __('Annuler') }}
+                        </a>
+
+                    </span>
+                </div>
             </div>
         </form>
     </div>
