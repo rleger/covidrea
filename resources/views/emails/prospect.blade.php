97733ea99961d6e926c1efe51a17d52ec5505b45
Romain Léger <legerrom@gmail.com>
2020-03-31 00:05:46 +0200

    new mail

--- resources/views/emails/prospect.blade.php
+++ resources/views/emails/prospect.blade.php
@@ -1,61 +1,49 @@
 @component('mail::message')
-{{-- # {{ $details['title'] }} --}}
 
-# Madame la Directrice / Monsieur le Directeur, Cher collègue, 
+# Madame, Monsieur le Directeur,
 
+[Covid moi un lit](https://covid-moi-un-lit.com) est la première application dédiée aux **professionnels de santé** à la recherche d’un **lit de réanimation** pour leurs malades COVID+. Trouver un lit, c'est sauver une vie.
 
-Covid moi un lit est la première application dédiée aux professionnels de santé
-à la recherche d’un lit de réanimation pour leurs malades COVID+.
-
-Cet outil GRATUIT et COLLABORATIF, développé par des médecins pour des médecins,
-propose un mode de communication réactif, efficient.
-
-L’objectif n’est pas de concurrencer ou de parasiter les systèmes actuels mis en place mais d’apporter une aide supplémentaire pour la gestion opérationnelle de cette crise.
+Cet outil GRATUIT et COLLABORATIF, développé par des médecins pour des médecins, propose un mode de communication réactif et efficient.
 
 {{-- $prospect->makeSignedUrl('register') --}}
 @component('mail::button', ['url' => $prospect->makeTemporarySignedUrl('register', 7, [ "prospect" => $prospect->id ]) ])
 Activer mon compte
 @endcomponent
 
-## FACILE À UTILISER:
 
-Covid moi un lit est accessible sur PC, mac et smartphone.
+## FACILE À UTILISER
 
-Vos chefs de services (ou référents) renseignent eux-mêmes les lits disponibles dans leur unité en 30 secondes chrono.
-La procédure simplissime leur permet d’actualiser plusieurs fois par jour l’état des lits pour une meilleure information de vos confrères et des centres de régulation.
+- Le praticien renseigne lui-même les lits disponibles dans son unité en **30 secondes chrono** depuis un PC, mac ou smartphone.
+- La procédure simplissime leur permet d’actualiser plusieurs fois par jour l’état des lits
 
+@component('mail::panel')
+    30 secondes chrono
+@endcomponent
 
-## ALLER À L’ESSENTIEL (et vite):
+## ALLER À L’ESSENTIEL (et vite)
 
-Vous disposez en un coup d’oeil d'un panorama géo-localisé des lits de réanimation disponibles
-autour de votre établissement.
+- Panorama **géolocalisé** des lits disponibles en un coup d'oeil
+- Données issues directement du terrain
 
-Avec Covid moi un lit les informations sont issues directement du terrain et sont instantanément disponibles et exploitables pour l’ensemble des acteurs de la chaine de prise en charge des malades (médecins, bed managers, centres de crise…)
+@component('mail::panel')
+    En un coup d'oeil
+@endcomponent
 
+## NON COMMERCIAL ET SECURISE
 
-## NON COMMERCIAL:
+- [Covid moi un lit](https://covid-moi-un-lit.com) est **entièrement gratuit**
+- Aucune donnée de santé n'est requise
 
-Covid moi un lit est entièrement gratuit et n’a en aucun cas été développé dans un but commercial. 
-Nous aidons simplement nos confrères à la gestion opérationnelle de la crise.
-Inutile de préciser que nous ne vendrons en aucun cas les adresses mails collectées.
-Aucune donnée de santé ne sont requises. Uniquement des chiffres.
+@component('mail::panel')
+    Gratuit et sans spam
+@endcomponent
 
-Nous pensons que notre outil peut aider nos confrères, nos patients, nos concitoyens.
+Pour toute question concernant l'outil ou son utilisation, vous pouvez nous joindre par email à [mail@covid-moi-un-lit.com](mailto:mail@covid-moi-un-lit.com).
 
-En permettant de gagner du temps aux médecins, Covid moi un lit vous aide à sauver des vies !
-
-Plus la communauté Covid moi un lit sera importante, plus l’information sera pertinente!
-
-
-Pour démarrer l'utilisation de Covid moi un lit au sein de votre établissement, il vous suffit de cliquer sur ce lien :  Activer mon compte
-
-Pour toute question concernant l'outil ou son utilisation, vous pouvez nous joindre par email à contact@covid-moi-un-lit.com ou 06 61 08 40 24
-
-En vous remerciant et restant à votre disposition.
-
+En vous remerciant de votre participation.
 
 Dr Romain Léger & Dr Vincent Bailly 
 
 Avec le soutien de: Hacking Health Besançon, Silicon Comté, CCI du Doubs
-
 @endcomponent
