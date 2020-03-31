2f548f1247e4f9211c73206b8f61bfa7de408459
Romain Léger <legerrom@gmail.com>
2020-03-31 18:12:06 +0200

    no index if not prod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+        @if(!env('production'))
+            <meta name="robots" content="noindex">
+            <meta name="googlebot" content="noindex">
+        @endif
+
         <!-- CSRF Token -->
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
