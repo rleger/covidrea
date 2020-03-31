f00250c5ba9607a5cf7ab0a8e5b37c372534f06f
Romain Léger <legerrom@gmail.com>
2020-03-31 10:57:04 +0200

    Only allow a user that administers a hospital to see the invite page

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -14,6 +14,8 @@ class InviteController extends Controller
     public function __construct()
     {
         $this->middleware('auth')->except('finalize');
+
+        $this->middleware('checkuseradministersahospital')->only('invite');
     }
 
     /**
---
+++ app/Http/Middleware/CheckUserAdministersAHospital.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Log;
+use Closure;
+
+class CheckUserAdministersAHospital
+{
+    /**
+     * Check that a user is attached to a service
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!auth()->user()->etablissement()->exists()) {
+            Log::error("User " . auth()->user()->id . " does not administers any hospital and cannot go further");
+            // Logout
+            auth()->logout();
+
+            // Send home
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
