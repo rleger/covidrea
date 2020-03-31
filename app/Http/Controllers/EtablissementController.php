068d722c224d695d0fe09a36c4e84fe11dfc8b08
bdahon <benjamin.dahon@gmail.com>
2020-03-31 12:12:58 +0200

    commented out everything for testing

--- app/Http/Controllers/EtablissementController.php
+++ app/Http/Controllers/EtablissementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use DB;
 use App\Etablissement;
 
+
 class EtablissementController extends Controller
 {
     /**
@@ -32,7 +33,7 @@ class EtablissementController extends Controller
 
         // Etablissement est soit la liste des établissements des services de l'utilisateur
         // soit l'établissement pour lequel l'utilisateur est référent (etablissement.user_id)
-        if (auth()->user()->services()->count()) {
+       /* if (auth()->user()->services()->count()) {
             // Position of the user's etablissement
             $etablissement = auth()->user()->services()->first()->etablissement;
         } else {
@@ -41,35 +42,38 @@ class EtablissementController extends Controller
 
         $lat = $etablissement->lat;
         $long = $etablissement->long;
-
+*/
         // Construct the sqlDistance query
-        $sqlDistance = DB::raw('( 6371 * acos( cos( radians('.$lat.') )
+       /* $sqlDistance = DB::raw('( 6371 * acos( cos( radians('.$lat.') )
             * cos( radians( etablissements.lat ) )
             * cos( radians( etablissements.long )
             - radians('.$long.') )
             + sin( radians('.$lat.') )
             * sin( radians( etablissements.lat ) ) ) )');
-
+*/
         // Construct the service count query
         // SELECT *, (SELECT count(*) FROM services WHERE services.etablissement_id=etablissements.`id`) AS cnt FROM etablissements
-        $sqlCount = DB::raw('( SELECT count(*) FROM services WHERE services.etablissement_id=etablissements.`id` )');
+       /* $sqlCount = DB::raw('( SELECT count(*) FROM services WHERE services.etablissement_id=etablissements.`id` )');
 
         $sqlPlace = DB::raw('( SELECT sum(place_disponible) + sum(place_bientot_disponible) FROM services WHERE services.etablissement_id=etablissements.`id` )');
-
+*/
         // Run the query
-        $paginator = DB::table('etablissements')
+       /* $paginator = DB::table('etablissements')
             ->select('etablissements.*')
             ->selectRaw("{$sqlDistance} AS distance, {$sqlCount} as service_count, {$sqlPlace} as places")
             // ->orderBy('service_count', 'DESC')
             // ->orderBy('places', 'DESC')
             ->orderBy('distance', 'ASC')
-            ->paginate(10);
+            ->paginate(10);*/
 
         // https://laracasts.com/discuss/channels/laravel/hydraterawfromquery-with-pagination
-        $etablissements = Etablissement::hydrate($paginator->items());
+        //$etablissements = Etablissement::hydrate($paginator->items());
+
+        $etablissements = Etablissement::with("service")->get(); //$this->repository->all();
+        //print_r($all);
 
         // return the view
-        return view('etablissement.index', compact('etablissements', 'paginator'));
+        return view('etablissement.index', compact('etablissements'));
     }
 
     /**
