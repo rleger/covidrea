0d1ab7333b007f71652254f3dfd32d82020cbb54
bdahon <benjamin.dahon@gmail.com>
2020-03-31 12:14:29 +0200

    added debug for sql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+       DB::listen(function ($query) {
+            var_dump([
+                $query->sql,
+           
+            ]);
+        });
+        
     }
 }
