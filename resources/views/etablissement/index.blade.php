c5c284af60988ba2c16e199e7d5242e48f215ace
bdahon <benjamin.dahon@gmail.com>
2020-03-31 12:14:01 +0200

    using new functions. first prototype of map

--- resources/views/etablissement/index.blade.php
+++ resources/views/etablissement/index.blade.php
@@ -5,13 +5,133 @@
 
 @section('content')
 
+
+<style>
+.custom-map-controls {
+    margin: 1rem;
+    font-family: Inter var, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    background-color: #5850EC;
+}
+
+.custom-map-controls label {
+    margin-right: 2rem;
+}
+
+.custom-map-controls input {
+    vertical-align: top;
+    margin-right: 0.25rem;
+}
+
+
+
+</style>
+
+
+<div id="map" class="google-map" style="width: 100%; height: 500px;"></div>
+<div id="map-filters-template"  style="display: none;">
+    <div class="p-2 bg-gray-100 text-sm custom-map-controls">
+        <input type="checkbox" name="map-filters-all" class="map-filters-all" onclick="filterMarkers('all')" checked>
+        <label for="map-filters-all">Tous</label>
+        
+        <input type="checkbox" name="map-filters-available" class="map-filters-available" onclick="filterMarkers('available')">
+        <label for="map-filters-available">Lits disponibles</label>
+        
+
+        <input type="checkbox" name="map-filters-soon" class="map-filters-soon" onclick="filterMarkers('soon')">
+        <label for="map-filters-soon">Lits bientôt disponibles</label>
+    </div>
+</div>
+
+
+<script>
+    var map;
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+        // france center: { lat: 46.227638, lng: 2.213749 },
+        center: { lat: 9.8303450, lng: 49.6575990 },
+        zoom: 6,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false
+    });
+
+    let filterMarkers = (type) => {
+        
+        if(type === "all") {
+            let checkbox = document.querySelector("#map-filters .map-filters-all");
+            markers.forEach(marker => marker.setVisible(checkbox.checked));
+            // uncheck others
+            document.querySelector("#map-filters .map-filters-available").checked = false;
+            document.querySelector("#map-filters .map-filters-soon").checked = false;
+
+        } else if (type === "available") {
+            let checkbox = document.querySelector("#map-filters .map-filters-available");
+            document.querySelector("#map-filters .map-filters-all").checked = false;
+
+            let soonStatus = document.querySelector("#map-filters .map-filters-soon").checked;
+            let availableStatus = document.querySelector("#map-filters .map-filters-available").checked;
+            markers.forEach(marker => {
+                marker.setVisible((soonStatus && marker._underlying.soon_available > 0) || (availableStatus && marker._underlying.available > 0));
+            });
+
+
+        } else {
+            let checkbox = document.querySelector("#map-filters .map-filters-soon");
+            document.querySelector("#map-filters .map-filters-all").checked = false;
+
+            let soonStatus = document.querySelector("#map-filters .map-filters-soon").checked;
+            let availableStatus = document.querySelector("#map-filters .map-filters-available").checked;
+            markers.forEach(marker => {
+                marker.setVisible((soonStatus && marker._underlying.soon_available > 0) || (availableStatus && marker._underlying.available > 0));
+            });
+
+        }
+    };
+
+
+    let etablissements = [];
+    let markers = [];
+    @foreach($etablissements as $key => $etablissement)
+    markers.push(
+        new google.maps.Marker({
+            position: { lat: {{ $etablissement->lat }}, lng: {{ $etablissement->long }} },
+            map: map,
+            icon: ["/images/map/logo-available.svg", "/images/map/logo-soon-available.svg", "/images/map/logo-unavailable.svg"][Math.floor(Math.random() * 3)],
+            _underlying: {
+                id: "{{ $etablissement->id }}",
+                soon_available: {{ $etablissement->service()->sum('place_bientot_disponible') }},
+                available: {{ $etablissement->service()->sum('place_disponible') }},
+                lat: "{{ $etablissement->lat }}",
+                lon: "{{ $etablissement->long }}"
+            }
+        })
+    );
+    @endforeach
+
+
+
+  
+    var div = document.createElement('div');
+    div.id = "map-filters";
+    div.innerHTML = document.getElementById("map-filters-template").innerHTML;
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(div);
+
+    // events
+    
+
+</script>
+
+
+  
+
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             {{-- Begining of list --}}
             <div class="bg-white shadow overflow-hidden sm:rounded-md">
                 <ul>
                     @foreach($etablissements as $key => $etablissement)
-                        <li class="{{ $key ? 'border-t border-gray-200' : ''}}">
+                    <li class="{{ $key ? 'border-t border-gray-200' : ''}}">
                             <a href="{{ $etablissement->service_count ? route('etablissement.show', $etablissement) : '#' }}" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
                                 <div class="px-4 py-4 sm:px-6">
                                     <div class="flex flex-wrap items-center justify-between">
@@ -23,13 +143,12 @@
                                         </div>
                                         <div class="sm:ml-2 flex-shrink-0 flex">
                                             <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                {{ $etablissement->service()->sum('place_disponible') }} disponibles
+                                                {{ $etablissement->numberOfAvailableBeds() }} disponibles
                                             </span>
 
                                             <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
-                                                {{ $etablissement->service()->sum('place_bientot_disponible') }} prochainement
+                                                {{ $etablissement->numberOfSoonAvailableBeds() }} prochainement
                                             </span>
-
                                         </div>
                                     </div>
                                     <div class="mt-2 sm:flex sm:justify-between">
@@ -67,30 +186,15 @@
                         </li>
                     @endforeach
                 </ul>
-                <div class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-                    <div class="flex-1 flex justify-between sm:hidden">
-                        <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {{ __('Previous') }}
-                        </a>
-                        <a href="{{ $paginator->nextPageUrl() }}" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                            {{ __('Next') }}
-                        </a>
-                    </div>
-                    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-                        <div>
-                            <p class="text-sm leading-5 text-gray-700">
-                                <span class="font-medium">{{ $paginator->total() }}</span> résultats
-                            </p>
-                        </div>
-                        <div>
-                            <span class="relative z-0 inline-flex shadow-sm">
-                                {{ $paginator->links() }}
-                            </span>
-                        </div>
-                    </div>
-                </div>
+
             </div>
             {{-- End of list --}}
+
+            
+   
+
         </div>
+
+        
     </div>
 @endsection
