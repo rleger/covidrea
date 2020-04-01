223ae2b67d59c503a270134351bbeee4f32fee49
Romain Léger <legerrom@gmail.com>
2020-04-01 21:10:02 +0200

    missing closing form tag

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -122,7 +122,7 @@
                                     <button type="button" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="inline-flex items-center w-full px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150">
                                         {{ __('Logout') }}
                                     </button>
-
+                                </form>
                             </div>
                         </a>
                     </div>
