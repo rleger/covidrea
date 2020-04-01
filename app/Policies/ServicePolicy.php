6cfd3a9f6b783e6f65a33632a6550d177df01775
Samantha Halfon Faw <samantha.halfon@gmail.com>
2020-04-01 13:00:52 +0200

    Policies: migrating policies from AuthServiceProviders to dedicated policy classes (#63)

---
+++ app/Policies/ServicePolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\Service;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ServicePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any services.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the service.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Service  $service
+     * @return mixed
+     */
+    public function view(User $user, Service $service)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create services.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the service.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Service  $service
+     * @return mixed
+     */
+    public function update(User $user, Service $service)
+    {
+        return $user->services()->get()->contains($service->id) || $service->etablissement->user_id == $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the service.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Service  $service
+     * @return mixed
+     */
+    public function delete(User $user, Service $service)
+    {
+        return $service->etablissement->user_id == $user->id;
+    }
+
+    /**
+     * Determine whether the user can restore the service.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Service  $service
+     * @return mixed
+     */
+    public function restore(User $user, Service $service)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the service.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Service  $service
+     * @return mixed
+     */
+    public function forceDelete(User $user, Service $service)
+    {
+        //
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                                             <a href="{{ route('home') }}" class="px-3 py-2 rounded-md text-sm font-medium focus:outline-none focus:text-white focus:bg-gray-700 {{ Route::is('home') ? 'bg-gray-900 text-white' : 'text-gray-300' }}">Tableau de bord</a>
                                             <a href="{{ route('etablissements.index') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 {{ Route::is('etablissements.index') ? 'bg-gray-900 text-white' : 'text-gray-300' }}">Lits disponibles</a>
                                             <a href="{{ route('user.services.edit', ['user' => auth()->user()->id]) }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 {{ Route::is('user.services.edit') ? 'bg-gray-900 text-white' : 'text-gray-300' }}">Mettre à jour mes lits</a>
-                                            @can('invite')
+                                            @can('invite', auth()->user())
                                                 <a href="{{ route('invite') }}" class="ml-4 px-3 py-2 rounded-md text-sm font-medium hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 {{ Route::is('invite') ? 'bg-gray-900 text-white' : 'text-gray-300' }}">Inviter des utilisateurs</a>
                                             @endcan
 
@@ -100,7 +100,7 @@
                                 <a href="{{ route('home') }}" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700">Tableau de bord</a>
                                 <a href="{{ route('etablissements.index') }}" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Lits disponibles</a>
                                 <a href="{{ route('user.services.edit', ['user' => auth()->user()->id]) }}" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Mettre à jour mes lits</a>
-                                @can('invite')
+                                @can('invite', auth()->user()))
                                     <a href="{{ route('invite') }}" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700">Inviter des collègues</a>
                                 @endcan
 
