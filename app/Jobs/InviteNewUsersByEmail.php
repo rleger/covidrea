0d1eb0b6d83163c708178539c4539b5b22286514
bdahon <benjamin.dahon@gmail.com>
2020-04-02 23:38:56 +0200

    Added confirmation email

--- app/Jobs/InviteNewUsersByEmail.php
+++ app/Jobs/InviteNewUsersByEmail.php
@@ -2,12 +2,15 @@
 
 namespace App\Jobs;
 
+use App\Etablissement;
 use Str;
 use Mail;
 use App\Invite;
 use App\Mail\InviteCreated;
 use Illuminate\Bus\Queueable;
 use App\Events\EmailWasSentToInvite;
+use App\Mail\InvitationConfirmation;
+use App\User;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -50,9 +53,12 @@ class InviteNewUsersByEmail implements ShouldQueue
     public function handle()
     {
         // Creating the Invite models
-        foreach($this->emails as $email) {
+        foreach ($this->emails as $email) {
             $this->createInviteModel($email);
         }
+
+        // once all the invitation emails have been sent, send a confirmation email to the etablissement user
+        $this->sendConfirmationEmail();
     }
 
     /**
@@ -77,7 +83,6 @@ class InviteNewUsersByEmail implements ShouldQueue
 
         $this->sendInvitationEmail($invite);
 
-        //@todo: send confirmation by email
     }
 
     /**
@@ -92,5 +97,17 @@ class InviteNewUsersByEmail implements ShouldQueue
 
         // Emit an event once an email has been sent
         event(new EmailWasSentToInvite($invite));
+    }
+
+    /**
+     * Send a confirmation email to the etablissement owner
+     *
+     * @param mixed $invite
+     */
+    protected function sendConfirmationEmail()
+    {
+        // send the email
+        $user = Etablissement::find($this->etablissement_id)->user();
+        Mail::to($user->email)->send(new InvitationConfirmation($this->etablissement_id, $this->emails));
     }
 }
