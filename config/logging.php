a50edd45d546c539ab5a1ecc86376edfa158be06
Romain Léger <legerrom@gmail.com>
2020-04-02 10:55:34 +0200

    removing slack from logging

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'flare', 'slack'],
+            'channels' => ['daily', 'flare'],
             'ignore_exceptions' => false,
         ],
 
