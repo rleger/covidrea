48780af9e89254e29bab74ca80ab1dd024eb63b6
Karim Ammor <karim.ammor@enablon.com>
2020-04-03 11:25:33 +0200

    fix merge conflict

--- resources/views/etablissement/index.blade.php
+++ resources/views/etablissement/index.blade.php
@@ -27,33 +27,12 @@
                                             </div>
                                             <div class="sm:ml-2 flex-shrink-0 flex">
                                                 <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                    {{ $etablissement->service()->sum('place_disponible') }} disponibles
+                                                    {{ $etablissement->place_disponible }} disponibles
                                                 </span>
 
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            {{-- Begining of list --}}
-            <div class="bg-white shadow overflow-hidden sm:rounded-md">
-                <ul>
-                    @foreach($etablissements as $key => $etablissement)
-                        <li class="{{ $key ? 'border-t border-gray-200' : ''}}">
-                            <a href="{{ $etablissement->service_count ? route('etablissement.show', $etablissement) : '#' }}" class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
-                                <div class="px-4 py-4 sm:px-6">
-                                    <div class="flex flex-wrap items-center justify-between">
-                                        <div class="pb-2 sm:pb-0 text-md sm:text-lg leading-6 font-medium text-indigo-600">
-                                            {{ $etablissement->name }}
-                                            <span class="text-sm sm:text-md text-gray-400">
-                                            ({{ $etablissement->service_count }} {{Str::plural('service', $etablissement->service_count)}})
-                                            </span>
-                                        </div>
-                                        <div class="sm:ml-2 flex-shrink-0 flex">
-                                            <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                {{ $etablissement->place_disponible }} disponibles
-                                            </span>
-
-                                            <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
-                                                {{ $etablissement->place_bientot_disponible }} prochainement
-                                            </span>
+                                                <span class="mr-2 px-2 inline-flex text-sm leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
+                                                    {{ $etablissement->place_bientot_disponible }} prochainement
+                                                </span>
 
                                             </div>
                                         </div>
@@ -82,7 +61,7 @@
                                                     </span>
                                                     <span>
                                                         {{-- <time datetime="2020-01-07">January 7, 2020</time> --}}
-                                                        {{ $etablissement->service()->orderBy('updated_at', 'DESC')->first()->updated_at->diffForHumans() }}
+                                                        {{ Carbon\Carbon::parse($etablissement->updated_at)->diffForHumans() }}
                                                     </span>
                                                 @endif
                                             </div>
