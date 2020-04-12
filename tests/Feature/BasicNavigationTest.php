aa7c806035a78e7af60a7b35a5e85a93ef9e26d8
Romain Léger <legerrom@gmail.com>
2020-04-12 17:02:48 +0200

    test

--- tests/Feature/BasicNavigationTest.php
+++ tests/Feature/BasicNavigationTest.php
@@ -18,6 +18,14 @@ class BasicNavigationTest extends TestCase
     }
 
     /** @test */
+    public function the_press_page_returns_200()
+    {
+        $response = $this->get('/presse');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
     public function a_user_can_access_login_page()
     {
         $response = $this->get('/login');
